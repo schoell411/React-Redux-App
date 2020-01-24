94f0feae5000a3eaec87a600cd81c88ca88074ec
Kevin Schoell <schoell411@gmail.com>
2020-01-24 02:08:22 -0600

    cleaned up file structure

--- react-redux-app/src/App.js
+++ react-redux-app/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 
@@ -8,28 +7,9 @@ import axios from 'axios';
 
 function App() {
 
-  axios
-    .get("https://api.gemini.com/v1/symbols")
-    .then(response =>{
-      console.log(response)
-    })
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>start building</h1>
     </div>
   );
 }
